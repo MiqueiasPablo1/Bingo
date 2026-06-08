ec95d3e0242000abb5492a421f34923579b99353
Miqueias <miqueiasmorais152@gmail.com>
2026-06-08 05:18:16 -0300

    "feat: melhoria em métodos marcarNaTabela e mostrarParticipantes"

--- src/model/entities/Sorteador.java
+++ src/model/entities/Sorteador.java
@@ -10,6 +10,38 @@ public class Sorteador {
     public Sorteador (){
     }
 
+    public void verificarGanhador(){
+        
+    }
+
+    public void marcarNaCartela (int pedra) {
+
+        for (int k = 0 ; k < jogadores.length; k++) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                   if (jogadores[k] != null) {
+                       if (jogadores[k].cartela.numeros[i][j] == pedra) {
+                           jogadores[k].cartela.marcados[i][j] = true;
+                       }
+                   }
+                }
+            }
+        }
+    }
+
+    public void mostrarParticipantes(){
+        for (Jogador i : jogadores) {
+            if (i != null) {
+                    System.out.print(i);
+            }
+
+        }
+    }
+
+    public void adicionarJogador (Jogador jogador, int contador) {
+        jogadores[contador] = jogador;
+    }
+
     public int sortearPedra(){
         Random random = new Random();
         return random.nextInt(101);
