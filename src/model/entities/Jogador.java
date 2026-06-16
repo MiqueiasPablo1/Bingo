3d8645cba7ddb4df23af31db7f08d9e237ca5d6a
Luc4sDv <diaslucasdv@gmail.com>
2026-06-16 15:11:08 -0300

    to string arrumado

--- src/model/entities/Jogador.java
+++ src/model/entities/Jogador.java
@@ -29,59 +29,42 @@ public class Jogador {
         if (linha == 0) {
             System.out.printf("%-20s", "==== B I N G O ====");
         }
-
         else if (linha == 1) {
             System.out.printf("%-20s",
                     String.format("Cod: %04d", codigo));
         }
-
         else if (linha == 2) {
             System.out.printf("%-20s",
                     "Jogador: " + nome);
         }
-
         else if (linha == 3) {
             System.out.printf("%-20s",
                     "Marcadas: " + marcador);
         }
-
         else {
 
-            int l = linha - 4;
+            int i = linha - 4;
 
-            for (int c = 0; c < 5; c++) {
+            for (int j = 0; j < 5; j++) {
 
-                if (marcados[l][c]) {
+                if (marcados[i][j]) {
 
                     System.out.print(AZUL);
-                    System.out.printf("%3d ", cartela[l][c]);
-                    System.out.print(RESET);
+                    System.out.printf("%3d ", cartela[i][j]);
 
-                } else {
-
-                    System.out.printf("%3d ", cartela[l][c]);
+                    System.out.printf("%3d ", cartela[i][j]);
                 }
             }
         }
     }
 
-    @Override
+
     public String toString() {
 
-        String texto = "";
-
-        texto += "==== B I N G O ====\n";
-        texto += "Código: 000" + codigo + "\n";
-        texto += "Jogador: " + nome + "\n";
-        texto += "Marcadas: " + marcador + "\n";
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                texto += cartela[i][j] + " ";
-            }
-            texto += "\n";
-        }
-
+        String texto = "==== B I N G O ====\n" +
+                "Jogador: " + nome + "\n" +
+                "Código 000" + codigo + "\n" +
+                "Marcadas: " + marcador;
         return texto;
     }
 
